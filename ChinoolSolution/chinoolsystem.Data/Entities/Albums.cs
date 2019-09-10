e09153e0637aafba8b7f62e3254e5884e06f4eef
ParcoGuan <51099933+ParcoGuan@users.noreply.github.com>
2019-09-09 20:28:49 -0600

    updata

---
+++ ChinoolSolution/chinoolsystem.Data/Entities/Albums.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chinoolsystem.Data.Entities
+{
+    public  class Albums
+    {
+        [Key]
+        public int ArtstId { get; set; }
+
+        public string Title { get; set; }
+        public int ReleaseYear { get; set; }
+        public int Albumid { get; set; }
+
+        private string _ReleaseLabel;
+        public string ReleaseLabel
+        {
+            get
+            {
+                return ReleaseLabel;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    _ReleaseLabel = null;
+                }
+                else
+                {
+                    _ReleaseLabel = value;
+                }
+            }
+
+        }
+        public virtual Artists Artists { get; set; }
+    }
+}
---
+++ ChinoolSolution/chinoolsystem.Data/Entities/Artists.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region addditional namespace
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace chinoolsystem.Data.Entities
+{
+    [Table("Artsties")]
+    public class Artists
+    {
+        [Key]
+        public int ArtstId { get; set; }
+        private string _Name;
+        
+        public string Name
+        {
+            get
+            {
+                return Name;
+            }
+            set
+            {
+                if(string.IsNullOrEmpty(value))
+                {
+                    _Name = null;
+                }
+                else
+                {
+                    _Name = value;
+                }
+            }
+
+        }
+        
+
+        public virtual ICollection<Albums> Albums { get; set; }
+
+
+
+
+    }
+}
