81cfd2a4aeb596f82cf0b8ce3cc7dafce4891bec
ParcoGuan <51099933+ParcoGuan@users.noreply.github.com>
2019-09-18 19:08:39 -0600

    ff

---
+++ ChinookSolution/ChinookSystem/BLL/AlbumController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using ChinookSystem.DAL;
+using ChinookSystem.Data.Entities;
+using System.ComponentModel;
+#endregion
+
+namespace ChinookSystem.BLL
+{
+    [DataObject]
+    public class AlbumController
+    {
+        public List<Album> Album_List()
+        {
+            using (var context = new ChinookContext())
+            {
+                return context.Albums.ToList();
+            }
+        }
+
+        public Album Album_Get(int albumid)
+        {
+            using (var context = new ChinookContext())
+            {
+                return context.Albums.Find(albumid);
+            }
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Select,false)]
+        public List<Album> Album_FindByArtist(int artistid)
+        {
+            using (var context = new ChinookContext())
+            {
+                var results = from x in context.Albums
+                              where x.ArtistId == artistid
+                              select x;
+               
+                return results.ToList();
+            }
+        }
+    }
+}
